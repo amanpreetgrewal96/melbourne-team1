eba2110b682e0e869bb339ae888824c202ae1a91
amanpreetgrewal96 <82488893+amanpreetgrewal96@users.noreply.github.com>
2021-06-05 13:39:06 +1000

    Add files via upload

--- result_system.sql
+++ result_system.sql
@@ -1,13 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.2.1
--- http://www.phpmyadmin.net
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 12, 2021 at 12:50 AM
--- Server version: 5.0.45
--- PHP Version: 5.2.5
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2021 at 05:36 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `result_system`
@@ -23,9 +31,7 @@ CREATE TABLE `rms_admin` (
   `id` int(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `pass` varchar(50) NOT NULL,
-  `isactive` varchar(50) NOT NULL,
-  KEY `id` (`id`),
-  KEY `id_2` (`id`)
+  `isactive` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -42,11 +48,10 @@ INSERT INTO `rms_admin` (`id`, `username`, `pass`, `isactive`) VALUES
 --
 
 CREATE TABLE `rms_class` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL,
   `name` varchar(512) NOT NULL,
-  `section` varchar(512) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `section` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rms_class`
@@ -62,11 +67,10 @@ INSERT INTO `rms_class` (`id`, `name`, `section`) VALUES
 --
 
 CREATE TABLE `rms_notification` (
-  `id` int(100) NOT NULL auto_increment,
+  `id` int(100) NOT NULL,
   `note` text NOT NULL,
-  `date` date NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rms_notification`
@@ -80,11 +84,34 @@ INSERT INTO `rms_notification` (`id`, `note`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `rms_query`
+--
+
+CREATE TABLE `rms_query` (
+  `id` int(100) NOT NULL,
+  `enrollment_id` varchar(100) NOT NULL,
+  `sname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `query` varchar(100) NOT NULL,
+  `response` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rms_query`
+--
+
+INSERT INTO `rms_query` (`id`, `enrollment_id`, `sname`, `email`, `query`, `response`) VALUES
+(2, '1233', 'supreet', 'supreet@gmail.com', 'result display', 'now you can check, solved'),
+(3, 'ERD002', 'jkjkkj', 'archana@gmail.com', 'test', 'test2');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `rms_result`
 --
 
 CREATE TABLE `rms_result` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL,
   `enrollment_id` varchar(255) NOT NULL,
   `name` varchar(500) NOT NULL,
   `fname` varchar(500) NOT NULL,
@@ -94,15 +121,16 @@ CREATE TABLE `rms_result` (
   `session` varchar(500) NOT NULL,
   `subjects` text NOT NULL,
   `marks` varchar(500) NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rms_result`
 --
 
-INSERT INTO `rms_result` (`id`, `enrollment_id`, `name`, `fname`, `class`, `section`, `mobile`, `session`, `subjects`, `marks`) VALUES
-(5, 'ERD002', 'jkjkkj', 'aman', '5th', 'B', '9876543210', 'Mar-21', 'Math, English, Science, Hindi', '23,23,55,60');
+INSERT INTO `rms_result` (`id`, `enrollment_id`, `name`, `fname`, `class`, `section`, `mobile`, `session`, `subjects`, `marks`, `status`) VALUES
+(5, 'ERD001', 'Supreet', 'abc', '5th', 'B', '9876543210', 'Mar-21', 'Math, English, Science, Hindi', '23,23,55,60', '1'),
+(6, 'ERD002', 'Archana', 'aaaa', '6th', 'B', '999999999', '2021', 'Math, English, Science, Hindi', '98,89,76,83', '1');
 
 -- --------------------------------------------------------
 
@@ -111,7 +139,7 @@ INSERT INTO `rms_result` (`id`, `enrollment_id`, `name`, `fname`, `class`, `sect
 --
 
 CREATE TABLE `rms_student_register` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL,
   `doad` date NOT NULL,
   `regno` varchar(100) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -125,17 +153,16 @@ CREATE TABLE `rms_student_register` (
   `section` varchar(10) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `notes` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `notes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rms_student_register`
 --
 
 INSERT INTO `rms_student_register` (`id`, `doad`, `regno`, `name`, `fname`, `mname`, `dob`, `mob1`, `mob2`, `address`, `class`, `section`, `email`, `password`, `notes`) VALUES
-(1, '2021-05-04', 'ERD001', 'sas', 'sasa', 'sasa', '2021-05-02', 'sasa', 'assa', 'assa                                          ', '5th', 'A', 'aman@gmail.com', '12345', 'asas'),
-(2, '2021-05-04', 'ERD002', 'jkjkkj', 'aman', 'sasa', '2021-04-27', '9876543210', '9876543210', 'asasasa                                                                             ', '5th', 'B', 'sdpbuilders20@gmail.com', '123', 'ds');
+(1, '2021-05-04', 'ERD001', 'Supreet', 'aaaaa', 'sasa', '2021-05-02', 'sasa', 'assa', 'assa                                          ', '5th', 'A', 'supreet@gmail.com', '12345', 'asas'),
+(2, '2021-05-04', 'ERD002', 'Archana', 'aaaa', 'sasa', '2021-04-27', '123456789', '123456789', 'asasasa                                                                             ', '5th', 'B', 'archana@gmail.com', '123', 'ds');
 
 -- --------------------------------------------------------
 
@@ -144,11 +171,10 @@ INSERT INTO `rms_student_register` (`id`, `doad`, `regno`, `name`, `fname`, `mna
 --
 
 CREATE TABLE `rms_subject` (
-  `id` int(50) NOT NULL auto_increment,
+  `id` int(50) NOT NULL,
   `class` varchar(50) NOT NULL,
-  `subjects` text NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `subjects` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rms_subject`
@@ -156,3 +182,95 @@ CREATE TABLE `rms_subject` (
 
 INSERT INTO `rms_subject` (`id`, `class`, `subjects`) VALUES
 (1, '5th', 'Math, English, Science, Hindi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rms_admin`
+--
+ALTER TABLE `rms_admin`
+  ADD KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- Indexes for table `rms_class`
+--
+ALTER TABLE `rms_class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rms_notification`
+--
+ALTER TABLE `rms_notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rms_query`
+--
+ALTER TABLE `rms_query`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rms_result`
+--
+ALTER TABLE `rms_result`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `rms_student_register`
+--
+ALTER TABLE `rms_student_register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rms_subject`
+--
+ALTER TABLE `rms_subject`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rms_class`
+--
+ALTER TABLE `rms_class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rms_notification`
+--
+ALTER TABLE `rms_notification`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `rms_query`
+--
+ALTER TABLE `rms_query`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rms_result`
+--
+ALTER TABLE `rms_result`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `rms_student_register`
+--
+ALTER TABLE `rms_student_register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `rms_subject`
+--
+ALTER TABLE `rms_subject`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
